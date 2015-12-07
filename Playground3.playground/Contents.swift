0363c47190f6181185b16401ca5c3d8b63c8ae11
tristangaebler <tgae6529@740s-pl-115.local>
2015-12-07 09:28:27 -0700

    Added some classes

--- Playground3.playground/Contents.swift
+++ Playground3.playground/Contents.swift
@@ -1,1 +1,38 @@
 //Objects 
+
+import Cocoa
+
+var notes = "Using classes in swift"
+
+class SimpleClass {
+    //Declaraion section
+    var name: String
+    var age: Int
+    var isTired: Bool
+    
+    init () {
+        name = String()
+        age = -99
+        isTired = true
+    }
+    
+    convenience init(name: String) {
+        self.init()
+        self.name = name
+    }
+    
+    convenience init(name: String, age: Int) {
+        self.init()
+        self.name = name
+        self.age = age
+    }
+    
+}
+var someInstance = SimpleClass()
+print(someInstance.name)
+
+var otherInstance = SimpleClass(name: "defr")
+print(otherInstance.name)
+
+var convenienceInstance = SimpleClass(name: "other", age: 4567)
+print(convenienceInstance.age)
